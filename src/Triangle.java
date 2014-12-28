7bc849c9675bb58bfc1a698efedf1a9ad02a9be4
Christoph Wong <wong.christoph@gmail.com>
2014-12-28 11:08:29 -1000

    draw one asterisk

---
+++ src/Triangle.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Christoph on 12/28/14.
+ */
+public class Triangle {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+        triangle.drawOneAsterisk();
+    }
+
+    private void drawOneAsterisk() {
+        System.out.println("*");
+    }
+}
