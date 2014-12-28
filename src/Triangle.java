6b53ed248c8676e46055b7aa6f825e268e1b93e5
Christoph Wong <wong.christoph@gmail.com>
2014-12-28 11:22:26 -1000

    made method to draw vertical line

--- src/Triangle.java
+++ src/Triangle.java
@@ -4,8 +4,18 @@
 public class Triangle {
     public static void main(String[] args) {
         Triangle triangle = new Triangle();
+        System.out.println("Drawing one asterisk");
         triangle.drawOneAsterisk();
+        System.out.println("Drawing 8 asterisks on a line");
         triangle.drawHorizontalLine(8);
+        System.out.println("Drawing 3 asterisks vertically");
+        triangle.drawVerticalLine(3);
+    }
+
+    private void drawVerticalLine(int lineHeight) {
+        for(int i = 0; i < lineHeight; i++){
+            System.out.println("*");
+        }
     }
 
     private void drawHorizontalLine(int lineLength) {
