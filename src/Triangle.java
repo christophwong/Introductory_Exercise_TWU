5562ca52cafec96d67d53d76001d7d321b31718f
Christoph Wong <wong.christoph@gmail.com>
2014-12-28 11:14:39 -1000

    draw horizontal line of asterisk

--- src/Triangle.java
+++ src/Triangle.java
@@ -5,6 +5,14 @@ public class Triangle {
     public static void main(String[] args) {
         Triangle triangle = new Triangle();
         triangle.drawOneAsterisk();
+        triangle.drawHorizontalline(8);
+    }
+
+    private void drawHorizontalline(int lineLength) {
+        for(int i = 0; i < lineLength; i++) {
+            System.out.print("*");
+        }
+        System.out.println("");
     }
 
     private void drawOneAsterisk() {
