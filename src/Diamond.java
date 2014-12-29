2380f0def9ee696e4bb7caf75f73bd8476b6d0bf
Christoph Wong <wong.christoph@gmail.com>
2014-12-28 17:18:22 -1000

    draw diamond with name method made

--- src/Diamond.java
+++ src/Diamond.java
@@ -3,7 +3,10 @@
  */
 abstract class Diamond {
     public static void main(String[] args) {
+        System.out.println("draw Diamond with n = 3");
         Diamond.drawDiamond(3);
+        System.out.println("draw diamond with name");
+        Diamond.drawDiamondWithName(3);
     }
 
     public static void drawDiamond(int triangleHeight) {
@@ -14,6 +17,26 @@ abstract class Diamond {
             Triangle.drawHorizontalLine(2 * i + 1, "*");
             System.out.println("");
         }
+
+        for (int i = triangleHeight; i < height; i++){
+            int bottomOffset = i;
+            int bottomStarCount = 2 * (height - i - 1) + 1;
+            Triangle.drawHorizontalLine(bottomOffset, " ");
+            Triangle.drawHorizontalLine(bottomStarCount, "*");
+            System.out.println("");
+        }
+    }
+
+    public static void drawDiamondWithName(int triangleHeight) {
+        int height = triangleHeight * 2 - 1;
+        for (int i = 0; i < triangleHeight - 1; i++) {
+            int offset = height - i - 1;
+            Triangle.drawHorizontalLine(offset, " ");
+            Triangle.drawHorizontalLine(2 * i + 1, "*");
+            System.out.println("");
+        }
+
+        System.out.println("Christoph");
 
         for (int i = triangleHeight; i < height; i++){
             int bottomOffset = i;
