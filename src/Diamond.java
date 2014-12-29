1809eddeaf1f3f8fceb7360a5a50ae464b69c249
Christoph Wong <wong.christoph@gmail.com>
2014-12-28 14:03:40 -1000

    added isosceles triangle method. also extracted println from printHorizontalLine method

---
+++ src/Diamond.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Christoph on 12/28/14.
+ */
+public class Diamond {
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
--- src/Triangle.java
+++ src/Triangle.java
@@ -7,16 +7,33 @@ public class Triangle {
         System.out.println("Drawing one asterisk");
         triangle.drawOneAsterisk();
         System.out.println("Drawing 8 asterisks on a line");
-        triangle.drawHorizontalLine(8);
+        triangle.drawHorizontalLine(8, "*");
+        System.out.println("");
         System.out.println("Drawing 3 asterisks vertically");
         triangle.drawVerticalLine(3);
         System.out.println("Drawing triangle with sides of 3 asterisks");
         triangle.drawTriangle(3);
+
+        System.out.println("print isosceles triangle");
+        triangle.drawIsosTriangle(3);
+
+
+
+    }
+
+    private void drawIsosTriangle(int height) {
+        for (int i = 0; i < height; i++) {
+            int offset = height - i - 1;
+            drawHorizontalLine(offset, " ");
+            drawHorizontalLine(2 * i + 1, "*");
+            System.out.println("");
+        }
     }
 
     private void drawTriangle(int sideLength) {
         for (int i = 1; i <= sideLength; i++) {
-            drawHorizontalLine(i);
+            drawHorizontalLine(i, "*");
+            System.out.println("");
         }
     }
 
@@ -26,11 +43,10 @@ public class Triangle {
         }
     }
 
-    private void drawHorizontalLine(int lineLength) {
+    private void drawHorizontalLine(int lineLength, String output) {
         for(int i = 0; i < lineLength; i++) {
-            System.out.print("*");
+            System.out.print(output);
         }
-        System.out.println("");
     }
 
     private void drawOneAsterisk() {
